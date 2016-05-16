1c56279a298ac75b93a9a0533d60d69079cd6298
Joseph Orbegoso Pea <joe@trusktr.io>
2016-05-15 22:59:36 -0700

    Link to files in the sidebar, since some don't have inner content and therefore we can't navigate to those pages.

--- views/js/page.js
+++ views/js/page.js
@@ -2,6 +2,11 @@
 
   $(document).foundation();
 
+  document.addEventListener('click', event => {
+    if (Array.from(document.querySelectorAll('#nav a')).includes(event.target))
+      document.location = event.target.href
+  })
+
   $(function(){
     $('pre code').addClass('language-javascript');
   //   $('#nav > ul > li > a[href$="'+location.pathname.substring(location.pathname.lastIndexOf('/')+1, location.pathname.length)+'"]').parent().addClass('active');
